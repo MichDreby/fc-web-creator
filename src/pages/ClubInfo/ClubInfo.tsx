5f0bc97f9d6a845bd6120e0448f3f4308a52e103
michdreby <mr.sincee@gmail.com>
2022-10-13 17:01:36 +0300

    feat(info-page): added title

--- src/pages/ClubInfo/ClubInfo.tsx
+++ src/pages/ClubInfo/ClubInfo.tsx
@@ -24,60 +24,64 @@ interface IFormValues {
 export const ClubInfo: React.FC = React.memo(() => {
   return (
     <div className={styles.container}>
-      <Formik
-        initialValues={{
-          [FORM_VALUES.NAME]: '',
-          [FORM_VALUES.VENUE]: '',
-          [FORM_VALUES.FOUNDED]: null,
-          [FORM_VALUES.CLUB_COLORS]: [''],
-          [FORM_VALUES.WEBSITE]: '',
-        }}
-        onSubmit={(
-          values: IFormValues,
-          // { setSubmitting }: FormikHelpers<IFormValues>,
-        ) => {
-          console.log('******\n', 'values', values)
-        }}
-      >
-        <Form>
-          <div className={styles.formContainer}>
-            <Input
-              fieldId={FORM_VALUES.NAME}
-              label={'Name'}
-              placeholder={'Name'}
-              containerStyle={styles.inputContainer}
-            />
-            <Input
-              fieldId={FORM_VALUES.FOUNDED}
-              label={'Founded'}
-              placeholder={'Founded'}
-              containerStyle={styles.inputContainer}
-            />
-            <Input
-              fieldId={FORM_VALUES.VENUE}
-              label={'Venue'}
-              placeholder={'Venue'}
-              containerStyle={styles.inputContainer}
-            />
-            <Input
-              fieldId={FORM_VALUES.CLUB_COLORS}
-              label={'Colors'}
-              placeholder={'Colors'}
-              containerStyle={styles.inputContainer}
-            />
-            <Input
-              fieldId={FORM_VALUES.WEBSITE}
-              label={'Website'}
-              placeholder={'Website'}
-            />
-            <Button
-              onClick={() => {}}
-              label={'Submit'}
-              containerStyle={styles.buttonContainer}
-            />
-          </div>
-        </Form>
-      </Formik>
+      <div className={styles.contentContainer}>
+        <h1>Football Club information</h1>
+
+        <div className={styles.formContainer}>
+          <Formik
+            initialValues={{
+              [FORM_VALUES.NAME]: '',
+              [FORM_VALUES.VENUE]: '',
+              [FORM_VALUES.FOUNDED]: null,
+              [FORM_VALUES.CLUB_COLORS]: [''],
+              [FORM_VALUES.WEBSITE]: '',
+            }}
+            onSubmit={(
+              values: IFormValues,
+              // { setSubmitting }: FormikHelpers<IFormValues>,
+            ) => {
+              console.log('******\n', 'values', values)
+            }}
+          >
+            <Form>
+              <Input
+                fieldId={FORM_VALUES.NAME}
+                label={'Name'}
+                placeholder={'Name'}
+                containerStyle={styles.inputContainer}
+              />
+              <Input
+                fieldId={FORM_VALUES.FOUNDED}
+                label={'Founded'}
+                placeholder={'Founded'}
+                containerStyle={styles.inputContainer}
+              />
+              <Input
+                fieldId={FORM_VALUES.VENUE}
+                label={'Venue'}
+                placeholder={'Venue'}
+                containerStyle={styles.inputContainer}
+              />
+              <Input
+                fieldId={FORM_VALUES.CLUB_COLORS}
+                label={'Colors'}
+                placeholder={'Colors'}
+                containerStyle={styles.inputContainer}
+              />
+              <Input
+                fieldId={FORM_VALUES.WEBSITE}
+                label={'Website'}
+                placeholder={'Website'}
+              />
+              <Button
+                onClick={() => {}}
+                label={'Submit'}
+                containerStyle={styles.buttonContainer}
+              />
+            </Form>
+          </Formik>
+        </div>
+      </div>
     </div>
   )
 })
