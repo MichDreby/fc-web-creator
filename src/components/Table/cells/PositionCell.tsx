a8ee24356c318c2ced19b76ad592e7c9e76d45aa
michdreby <mr.sincee@gmail.com>
2022-12-02 17:08:26 +0300

    feat(table): added initial position picker drop down flow

--- src/components/Table/cells/PositionCell.tsx
+++ src/components/Table/cells/PositionCell.tsx
@@ -1,15 +1,67 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
+import Dropdown, { Option } from 'react-dropdown'
 
 import { CellProps } from '../types'
 
+import 'react-dropdown/style.css'
+import styles from './PositionCell.styles.module.css'
+
+export type PlayerPosition =
+  | 'goalkeeper'
+  | 'defender'
+  | 'midfielder'
+  | 'striker'
+  | 'manager'
+  | 'coach'
+  | 'stuff'
+
+const options = [
+  {
+    value: 'goalkeeper',
+    label: 'goalkeeper',
+  },
+  {
+    value: 'defender',
+    label: 'defender',
+  },
+  {
+    value: 'midfielder',
+    label: 'midfielder',
+  },
+  {
+    value: 'striker',
+    label: 'striker',
+  },
+  {
+    value: 'manager',
+    label: 'manager',
+  },
+  {
+    value: 'coach',
+    label: 'coach',
+  },
+  {
+    value: 'stuff',
+    label: 'stuff',
+  },
+]
+
 export const PositionCell: FC<CellProps<string>> = ({ isRowEditable }) => {
+  const [value, setValue] = useState<Option>(options[1])
+  const handleOnChange = (option: Option) => {
+    setValue(option)
+  }
+
   return (
-    <div
-      style={{
-        backgroundColor: isRowEditable ? 'red' : 'white',
-      }}
-    >
-      PositionCell
+    <div className={styles.container}>
+      <Dropdown
+        className={styles.dropdown}
+        disabled={!isRowEditable}
+        options={options}
+        onChange={handleOnChange}
+        value={value}
+        placeholder="Select an option"
+      />
     </div>
   )
 }
