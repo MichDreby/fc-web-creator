a7aed6c28e1296afdaa6ff735c8c6a9dfa339ad7
michdreby <mr.sincee@gmail.com>
2022-12-05 15:49:31 +0300

    feat(structure): added aliases for api, interfaces

--- craco.config.js
+++ craco.config.js
@@ -5,10 +5,13 @@ module.exports = {
   webpack: {
     alias: {
       '@assets': path.resolve(__dirname, 'src/assets'),
+      '@api': path.resolve(__dirname, 'src/api'),
       '@components': path.resolve(__dirname, 'src/components'),
       '@constants': path.resolve(__dirname, 'src/constants'),
       '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@interfaces': path.resolve(__dirname, 'src/interfaces'),
       '@pages': path.resolve(__dirname, 'src/pages'),
+      '@services': path.resolve(__dirname, 'src/services'),
       '@types': path.resolve(__dirname, 'src/types'),
       '@utils': path.resolve(__dirname, 'src/utils'),
     },
