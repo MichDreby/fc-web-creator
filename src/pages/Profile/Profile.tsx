7f212812887d7b55f1942195435d71da26079eba
michdreby <mr.sincee@gmail.com>
2022-12-07 13:01:07 +0300

    feat(styles): remade container style for profile, users, settings pages

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,9 +1,3 @@
 import styles from './styles.module.css'
 
-export const Profile = () => (
-  <div className={styles.container}>
-    <div className={styles.contentContainer}>
-      <p>Profile</p>
-    </div>
-  </div>
-)
+export const Profile = () => <div className={styles.container}>Profile</div>
